a3448a05eb888f40c2a2141c1d2bf7a81838fd43
yuma11 <11yuma20@gmail.com>
2020-01-04 22:41:23 +0900

    fix:ハンバーガーメニュー

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,16 +1,19 @@
 $(function() {
   // ハンバーガーメニュー
-  let hamburger = $('.menu-trigger');
-  let sidebar = $('.sidebar');
-  hamburger.click(function() {
+  let hamburger  = $('.menu-trigger');
+  let sidebar    = $('.sidebar');
+  let overlayTop = $('.overlay-top');
+  $(".menu-trigger, .overlay-top").click(function() {
     hamburger.toggleClass('active');
     if(sidebar.css('width') == '400px') {
       if(hamburger.hasClass('active')) {
         $('.sidebar').css('transform', 'translateX(0)');
         $("body").css('overflow','hidden');
+        overlayTop.fadeIn();
       } else {
         $('.sidebar').css('transform', 'translateX(-400px)');
         $("body").css('overflow','auto');
+        overlayTop.fadeOut();
       }
     } else {
       if(hamburger.hasClass('active')) {
