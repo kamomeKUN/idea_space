b730e58267ab244549fc87592da70d33e807ba45
yuma11 <11yuma20@gmail.com>
2020-01-04 17:11:15 +0900

    カードタイトルのボーダー色変更

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -37,44 +37,54 @@ $(function() {
   let categories = $('.card__other--category');
 
   categories.each(function(i, cat) {
-    let cardTop = $(cat).closest('.card').find('.card__top');
-    let catName = $(cat).text().trim();
+    let cardTop =   $(cat).closest('.card').find('.card__top');
+    let cardTitle = $(cardTop).find('.card__top--title');
+    let catName =   $(cat).text().trim();
     switch(catName) {
       case 'webサイト':
         $(cat).css('background', '#2196F3');
         $(cardTop).css('border', '7px solid #2196F3');
+        $(cardTitle).css('border-bottom', '2px dotted #2196F3');
         break;
       case 'アプリ':
         $(cat).css('background', '#1976D2');
         $(cardTop).css('border', '7px solid #1976D2');
+        $(cardTitle).css('border-bottom', '2px dotted #1976D2');
         break;
       case 'ゲーム':
         $(cat).css('background', '#f1c40f');
         $(cardTop).css('border', '7px solid #f1c40f');
+        $(cardTitle).css('border-bottom', '2px dotted #f1c40f');
         break;
       case '音楽':
         $(cat).css('background', '#E91E63');
         $(cardTop).css('border', '7px solid #E91E63');
+        $(cardTitle).css('border-bottom', '2px dotted #E91E63');
         break;
       case 'グルメ':
         $(cat).css('background', '#e67e22');
         $(cardTop).css('border', '7px solid #e67e22');
+        $(cardTitle).css('border-bottom', '2px dotted #e67e22');
         break;
       case '暮らし':
         $(cat).css('background', '#1abc9c');
         $(cardTop).css('border', '7px solid #1abc9c');
+        $(cardTitle).css('border-bottom', '2px dotted #1abc9c');
         break;
       case 'ビジネス':
         $(cat).css('background', '#283593');
         $(cardTop).css('border', '7px solid #283593');
+        $(cardTitle).css('border-bottom', '2px dotted #283593');
         break;
       case 'オフライン':
         $(cat).css('background', '#34495e');
         $(cardTop).css('border', '7px solid #34495e');
+        $(cardTitle).css('border-bottom', '2px dotted #34495e');
         break;
       case 'その他':
         $(cat).css('background', '#95a5a6');
         $(cardTop).css('border', '7px solid #95a5a6');
+        $(cardTitle).css('border-bottom', '2px dotted #95a5a6');
         break;
     }
   })
